4755d71d194656fd7086d3d6fd18c61460375fdd
Jaroslav Pesek <pesekja8@fit.cvut.cz>
2026-01-05 13:03:03 +0100

    devel: add nix

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "IPFIXCol2 Devel flake";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  inputs.systems.url = "github:nix-systems/default";
+  inputs.netmonpkgs.url = "github:jaroslavpesek/netmonpkgs";
+  inputs.flake-utils = {
+    url = "github:numtide/flake-utils";
+    inputs.systems.follows = "systems";
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, netmonpkgs, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages.default = pkgs.callPackage ./package.nix {
+          libfds = netmonpkgs.packages.${system}.libfds;
+          nemea-framework = netmonpkgs.packages.${system}.nemea-framework;
+        };
+
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.bashInteractive
+            pkgs.nixd
+            pkgs.nixpkgs-fmt
+            pkgs.cmake
+            pkgs.gcc
+            pkgs.docutils
+            pkgs.libxml2
+            pkgs.zlib
+            pkgs.rdkafka
+            pkgs.lz4
+            netmonpkgs.packages.${system}.libfds
+            netmonpkgs.packages.${system}.nemea-framework
+          ];
+        
+        shellHook = ''
+          echo "Welcome to IPFIXCol2 development environment 🔥"
+        '';
+        };
+      }
+    );
+}
